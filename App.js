6b6e2788e0576e98b209e9720c5b9945a2035955
Idayat03 <idayat.noufou@ynov.com>
2023-01-27 11:30:57 +0100

    création de components , sreens et index.js Co-authored-by: alexandre-piron <alexandre-piron@users.noreply.github.com>

--- App.js
+++ App.js
@@ -1,10 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { Button } from '@rneui/base';
 
 export default function App() {
+  const AwesomeButton = () => (<Button title='Welcome'/>)
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <AwesomeButton></AwesomeButton>
       <StatusBar style="auto" />
     </View>
   );
