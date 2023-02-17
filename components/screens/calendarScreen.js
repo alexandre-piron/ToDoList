b6ece49532d7ab80ab2708be346c2dadad7ae10c
alexandre-piron <alexandre.piron01@etu.umontpellier.fr>
2023-02-17 15:49:33 +0100

    essai de marquage sur le calendrier
    Il y a une erreur je commit donc après la présentation

--- components/screens/calendarScreen.js
+++ components/screens/calendarScreen.js
@@ -1,12 +1,30 @@
-import React from 'react';
-import {SafeAreaView, StyleSheet} from 'react-native';
+import { React, useState } from 'react';
+import {SafeAreaView, StyleSheet, FlatList, View } from 'react-native';
 import { Calendar } from 'react-native-calendars';
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 function CalendarScreen() {
+  const [itemList,setItemList] = useState([]);
+  const datesMarked = {}
+  const getItem = async () => {
+    try {
+        const jsonlist =  await AsyncStorage.getItem('tasks')
+        setItemList(jsonlist != null ? JSON.parse(jsonlist) : null);
+    } catch(e) {
+        console.log(e);
+    }
+    
+}
+getItem();
+itemList.forEach(item => {
+  console.log(datesMarked)
+  datesMarked[item['date']] = { marked: true };
+});
+//console.log(datesMarked);
   return (
-    <SafeAreaView style={styles.container}>
-      <Calendar onDayPress={(day) => console.log('Selected date: ', day) }/>
-    </SafeAreaView>
+      <SafeAreaView style={styles.container}>
+        <Calendar onDayPress={(day) => console.log('Selected date: ', day) } markedDates={datesMarked}/>
+      </SafeAreaView>
   );
 };
 
